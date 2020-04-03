fd8591466a4cb02bd9dba17fcf19b006c99d2e0b
Siddharth Lal <34659233+siddharthlal25@users.noreply.github.com>
2020-04-03 13:36:40 +0530

    image resizing to 416x416

---
+++ processing/resize.py
@@ -0,0 +1,15 @@
+import cv2
+import glob
+
+images = glob.glob("*.png")
+
+i = 1
+for x in images:
+
+    img = cv2.imread(x, -1)
+
+    re = cv2.resize(img, (416, 416))
+
+    cv2.imwrite("resized_" + str(i) + ".png", re)
+    i = i + 1
+end
