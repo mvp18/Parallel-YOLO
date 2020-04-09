970424810ca08637e33111b08baba4110f1ff7ba
Arib Alam <aribalam64@gmail.com>
2020-04-09 18:59:50 +0530

    added load function

--- layers-old/convolution.cu
+++ layers-old/convolution.cu
@@ -435,6 +435,34 @@ public:
     
     }
 
+    bool load_params(const char* fileprefix) {
+        
+        // get full filenames from the file prefix provided
+        std::string param_kernel_file = std::string(fileprefix) + ".bin";
+        std::string param_bias_file = std::string(fileprefix) + ".bias.bin";
+        
+        // reading the weights from the file
+        FILE *fp = fopen(param_kernel_file.c_str(), "rb");
+        if (!fp) {
+            printf("FILE ERROR: Cannot open file %s\n", param_kernel_file.c_str());
+            return false;
+        }
+        fread(&cpu_param_kernel[0], sizeof(float), in_channels * kernel_size * kernel_size * out_channels, fp);
+        fclose(fp);
+    
+        // reading the bias from the file
+        fp = fopen(param_bias_file.c_str(), "rb");
+        if (!fp) {
+            printf("FILE ERROR: Cannot open file %s\n", param_bias_file.c_str());
+            return false;
+        }
+        fread(&cpu_param_bias[0], sizeof(float), out_channels, fp);
+        fclose(fp);
+    
+        return true;
+    
+    }
+
 };
 
 void test_forward() {
