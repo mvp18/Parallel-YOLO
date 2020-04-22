72f80637a6986bcb25932deb894191bd574595ce
Siddharth Lal <34659233+siddharthlal25@users.noreply.github.com>
2020-04-22 22:15:50 +0530

    Removed virtual environment

--- main.sh
+++ main.sh
@@ -3,15 +3,11 @@ mkdir weights
 rm -rf build
 rm -rf data
 rm -rf eval
-virtualenv env
-source env/bin/activate
-pip install -r requirements.txt
 cd processing
 python resize.py
 bash get_standard_annotations.sh
 cd ../scripts
 python create_targets.py standard
-deactivate
 cd ..
 mkdir weights
 mkdir build
