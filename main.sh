67957d7fa82c6b2bfa7505fbad780e1aa6a1ebec
kshivamraj32 <kshivamraj32@gmail.com>
2020-04-22 15:04:54 +0530

    Update and rename create_standard_data.sh to main.sh

--- main.sh
+++ main.sh
@@ -1,5 +1,15 @@
+rm -rf weights
+rm -rf build
+rm -rf data
 cd processing
 python resize.py
 bash get_standard_annotations.sh
 cd ../scripts
 python create_targets.py standard
+cd ..
+mkdir weights
+mkdir build
+cd build
+cmake ..
+make
+
